9da84d7d988609bc0592860ac4ea8c74f45b9e19
Samirul Alam <samirulalam372@gmail.com>
2020-11-10 03:12:25 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
+import { Button,FormControl,Input,InputLabel } from '@material-ui/core';
+import Todo from './Todo';
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
+  const [todos,setTodos]=useState([]);
+  const [input,setInput]=useState("");
+  useEffect(() => {
+    db.collection("todos").orderBy("timestamp","desc").onSnapshot(snapshot=>{
+      setTodos(snapshot.docs.map(doc=>({id:doc.id,todo:doc.data().todo})))
+    })
+  }, [])
+  const addTodo=(event)=>{
+      event.preventDefault();
+      db.collection("todos").add({
+        todo:input,
+        timestamp:firebase.firestore.FieldValue.serverTimestamp()
+      })
+      setTodos([...todos,input]);
+      setInput("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <form>
+          <FormControl>
+              <InputLabel>Write a Todo</InputLabel>
+              <Input value={input} onChange={event => setInput(event.target.value)}/>
+          </FormControl>
+          <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+              Add todo
+          </Button>
+          {/* <button type="submit" onClick={addTodo}>Add todo</button> */}
+      </form>
+      <ul>
+        {todos.map(todo =>(
+          <Todo todo={todo}/>
+          //<li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
